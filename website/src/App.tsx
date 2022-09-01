15740789fd8f69f3f0c7710047bf9a5404cfd490
Elio Di Nino <52972357+ElioDiNino@users.noreply.github.com>
2022-09-01 00:14:00 -0700

    Support for mobile layout

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import './App.css';
 
 import { Routes, Route, Navigate } from "react-router-dom";
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { ThemeProvider, createTheme, responsiveFontSizes } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import { useMediaQuery } from 'react-responsive';
 
 import Layout from './layouts/MainLayout';
 import HomePage from "./pages/HomePage/HomePage";
@@ -11,9 +12,14 @@ import NoPage from "./pages/404Page/NoPage";
 import LinksPage from "./pages/LinksPage/LinksPage";
 import ContactPage from "./pages/ContactPage/ContactPage";
 
+// Title in the browser
 const siteTitle = "Elio Di Nino";
 
-const theme = createTheme({
+// Specifying what size to consider mobile
+const mobileSize = 600;
+
+// Customized MUI theme
+let theme = createTheme({
   palette: {
     mode: 'dark',
   },
@@ -23,17 +29,29 @@ const theme = createTheme({
     }
   },
 });
+theme = responsiveFontSizes(theme);
 
+// Main app component
 function App() {
+  // For layouts on different devices
+  const isMobile = useMediaQuery({
+    query: '(max-width: ' + mobileSize + 'px)'
+  })
+  const isDesktopOrLaptopOrTablet = useMediaQuery({
+    query: '(min-width: ' + mobileSize + 'px)'
+  })
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Routes>
+        {/* Hide the footer and header on the links page */}
         <Route element={<Layout hideHeaderPaths={['/links']} hideFooterPaths={['/links']} />}>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<HomePage isMobile={isMobile} isDesktopOrLaptopOrTablet={isDesktopOrLaptopOrTablet} />} />
           <Route path="/home" element={<Navigate to="/" />} />
           <Route path="/links" element={<LinksPage pageTitle="Links" siteTitle={siteTitle} />} />
-          <Route path="/contact" element={<ContactPage pageTitle="Contact" siteTitle={siteTitle} />} />
+          <Route path="/contact" element={<ContactPage pageTitle="Contact" siteTitle={siteTitle} isMobile={isMobile} isDesktopOrLaptopOrTablet={isDesktopOrLaptopOrTablet} />} />
+          {/* 404 Page */}
           <Route path="*" element={<NoPage pageTitle="404" siteTitle={siteTitle} />} />
         </Route>
       </Routes>
