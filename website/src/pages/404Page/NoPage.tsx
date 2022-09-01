3c16bf03c3133629fd7808fddf1d2e8ce58dbf35
Elio Di Nino <52972357+ElioDiNino@users.noreply.github.com>
2022-09-01 00:14:29 -0700

    Update 404 page

--- website/src/pages/404Page/NoPage.tsx
+++ website/src/pages/404Page/NoPage.tsx
@@ -6,10 +6,10 @@ import './NoPage.css';
 const NoPage = (props: any) => {
     document.title = props.pageTitle + " - " + props.siteTitle;
     return <>
-        <Box sx={{ verticalAlign: 'middle', height: "59vh" }}>
-            <Paper sx={{ width: "fit-content", p: 6, borderRadius: 10, mx: "auto", my: 12 }}>
-                <Typography variant="h1">404</Typography>
-                <Typography variant="h6">Whoops, it looks like you've hit a dead end!</Typography>
+        <Box sx={{ verticalAlign: 'middle', pb: 20 }}>
+            <Paper sx={{ width: "fit-content", maxWidth: "90%", p: 6, borderRadius: 10, mx: "auto", my: 12 }}>
+                <Typography variant="h1" sx={{ my: 2 }}>404</Typography>
+                <Typography variant="h6" sx={{ my: 1 }}>Whoops, it looks like you've hit a dead end!</Typography>
                 <Button href="/" sx={{ m: 1, fontSize: 18 }}>
                     Take Me Home
                 </Button>
