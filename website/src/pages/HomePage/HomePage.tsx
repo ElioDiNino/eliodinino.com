377774dc09f09b84a5d307bf032d82c497b2b1e3
Elio Di Nino <52972357+ElioDiNino@users.noreply.github.com>
2023-03-07 13:07:38 -0800

    Update home page text

--- website/src/pages/HomePage/HomePage.tsx
+++ website/src/pages/HomePage/HomePage.tsx
@@ -78,19 +78,19 @@ const HomePage = (props: any) => {
                 <Paper sx={{ width: paperWidth, mx: "auto", px: 6, py: 4, my: 4, maxWidth: 1100, borderRadius: 4 }}>
                     <Typography variant="h4" sx={{ mb: 2 }}>About Me</Typography>
                     <Typography variant="body1" align="left">
-                        I am a driven UBC Engineering student interested in computers and software. In my free time, I enjoy pursuing personal programming projects, cycling, playing soccer, learning about new technologies and <a href="https://photography.eliodinino.com" target="_blank" rel="noreferrer">taking landscape photos</a>.
+                        I am a driven UBC Computer Engineering student interested in computer hardware and software. In my free time, I enjoy pursuing personal programming projects, cycling, playing soccer, learning about new technologies and <a href="https://photography.eliodinino.com" target="_blank" rel="noreferrer">taking landscape photos</a>.
                     </Typography><br />
                     <Typography variant="body1" align="left">
                         One of my values is to always continue learning. To uphold this, I pursue projects that require me to learn new skills and I seek new experiences, often outside of my comfort zone. I also look to meet new people and challenge myself whenever I can. I believe that this continuous learning and seeking of challenges is important to my growth and adaptability.
                     </Typography><br />
                     <Typography variant="body1" align="left">
-                        My hard-working nature and natural leadership skills make me a valuable team member who can both listen and lead if needed. Furthermore, my attention to detail and organizational skills contribute to the timely completion of high-quality deliverables. As a result of these core attributes, along with my past experiences and promising future, I was awarded full-ride scholarships to both UBC and McMaster University.
+                        My hard-working nature and natural leadership skills make me a valuable team member who can both listen and lead if needed. Furthermore, my attention to detail and organizational skills contribute to the timely completion of high-quality deliverables. As a result of these core attributes, along with my past experiences, I was awarded full-ride scholarships to both UBC and McMaster University.
                     </Typography><br />
                     <Typography variant="h5" align="left">
                         🎯 Professional Goal
                     </Typography><br />
                     <Typography variant="body1" align="left">
-                        I hope to use my current and future skills to pursue a career in the Computer Engineering field. To get there I am looking to try many different roles through co-ops and volunteering opportunities at UBC. Following this, I will seek a career that I am passionate about and one that makes a positive impact on the world.
+                        I plan to use my current and future skills to pursue a career in the Computer Engineering field. To get there I am trying many different roles through co-ops and volunteering opportunities at UBC. Following this, I will seek a career that I am passionate about and one that makes a positive impact on the world.
                     </Typography>
                     <Divider sx={{ my: 4, borderBottomWidth: 1.5, color: "#ffffff", borderColor: "#ffffff" }} />
                     <Typography variant="h4" sx={{ mt: 3, mb: 2 }}>Projects</Typography>
