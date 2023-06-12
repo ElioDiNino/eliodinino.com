ee0e2b0ccaa601acd4b0bad5a7c75e2c56a01658
Elio Di Nino <52972357+ElioDiNino@users.noreply.github.com>
2023-06-11 23:27:24 -0400

    Custom tests

---
+++ website/cypress/e2e/home.cy.ts
@@ -0,0 +1,33 @@
+/// <reference types="cypress" />
+
+describe('Home Page', () => {
+    beforeEach(() => {
+        cy.visit('https://eliodinino.com/')
+
+        cy.eyesOpen({
+            // The name of the application under test
+            appName: 'Personal Website',
+            
+            // The name of the test case for the given application
+            testName: Cypress.currentTest.title,
+        })
+    })
+
+    it('Home Page Layout', () => {
+        cy.contains('Resume')
+        cy.contains('Links')
+        cy.contains('GitHub')
+
+        cy.eyesCheckWindow({
+            tag: "Home Page",
+            target: 'window',
+            fully: true,
+            matchLevel: 'Layout'
+        });
+    })
+
+    afterEach(() => {
+        // Close Eyes to tell the server it should display the results
+        cy.eyesClose()
+    })
+})
---
+++ website/cypress/e2e/links.cy.ts
@@ -0,0 +1,32 @@
+/// <reference types="cypress" />
+
+describe('Links Page', () => {
+    beforeEach(() => {
+        cy.visit('https://eliodinino.com/links')
+
+        cy.eyesOpen({
+            // The name of the application under test
+            appName: 'Personal Website',
+            
+            // The name of the test case for the given application
+            testName: Cypress.currentTest.title,
+        })
+    })
+
+    it('Links Page Layout', () => {
+        cy.eyesCheckWindow({
+            tag: "Links Page",
+            target: 'window',
+            fully: true,
+        });
+
+        cy.contains('Website')
+        cy.contains('Contact')
+        cy.contains('Resume')
+    })
+
+    afterEach(() => {
+        // Close Eyes to tell the server it should display the results
+        cy.eyesClose()
+    })
+})
