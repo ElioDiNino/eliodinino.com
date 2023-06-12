a1cd1fbba0b2c3fdac4e794ff3a0ea17062b1906
Elio Di Nino <52972357+ElioDiNino@users.noreply.github.com>
2023-06-11 23:26:47 -0400

    Base testing files

---
+++ website/cypress.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "cypress";
+import eyesPlugin from '@applitools/eyes-cypress' 
+
+export default eyesPlugin(defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+}));
