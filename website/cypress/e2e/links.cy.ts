92e385aa11ba9ceb96999be37681d1125bfe548f
Elio Di Nino <52972357+ElioDiNino@users.noreply.github.com>
2023-08-14 21:51:19 -0700

    Add further checks and delays

--- website/cypress/e2e/links.cy.ts
+++ website/cypress/e2e/links.cy.ts
@@ -4,7 +4,15 @@ describe('Links Page', () => {
     beforeEach(() => {
         cy.visit('/links')
         // Wait up to 10 seconds for the background image to load
-        cy.get('.background', { timeout: 10000 }).should('be.visible')
+        cy.get('.background', { timeout: 10000 })
+          .should('be.visible')
+          .then($el => {
+            const url = $el.css('background-image').match(/url\("(.*)"\)/)[1]
+            cy.request({url, failOnStatusCode: false})
+              .its('status')
+              .should('eq', 200)
+          })
+          .wait(1000)
 
         cy.eyesOpen({
             // The name of the application under test
