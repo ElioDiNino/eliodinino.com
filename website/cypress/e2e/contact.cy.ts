45d9da5d4e9c4e3faad8aa04afa947be1d8c92e9
Elio Di Nino <52972357+ElioDiNino@users.noreply.github.com>
2024-06-09 00:19:21 -0400

    fix(contact): don't use applitools on both tests

--- website/cypress/e2e/contact.cy.ts
+++ website/cypress/e2e/contact.cy.ts
@@ -3,17 +3,13 @@
 describe('Contact Page', () => {
     beforeEach(() => {
         cy.visit('/contact')
-
-        cy.eyesOpen({
-            // The name of the application under test
-            appName: 'Personal Website',
-            
-            // The name of the test case for the given application
-            testName: Cypress.currentTest.title,
-        })
     })
 
     it('Contact Page Layout', () => {
+        cy.eyesOpen({
+            appName: 'Personal Website',
+            testName: Cypress.currentTest.title,
+        })
         cy.eyesCheckWindow({
             tag: "Contact Page",
             target: 'window',
@@ -21,17 +17,12 @@ describe('Contact Page', () => {
         });
 
         cy.contains('Submit')
+        cy.eyesClose()
     })
 
-    it ('Contact Page Form', () => {
+    it('Contact Page Form', () => {
         cy.get('input[name="user_name"]').type('Elio Di Nino').should('have.value', 'Elio Di Nino')
         cy.get('input[name="user_email"]').type('test@eliodinino.com').should('have.value', 'test@eliodinino.com')
         cy.get('textarea[name="message"]').type('This is a test message').should('have.value', 'This is a test message')
-    })
-
-
-    afterEach(() => {
-        // Close Eyes to tell the server it should display the results
-        cy.eyesClose()
     })
 })
