9502cc36f9d92abdf9135ba9f8aaa8854221e028
Elio Di Nino <52972357+ElioDiNino@users.noreply.github.com>
2024-08-18 19:40:36 -0400

    fix(constants): instagram url

--- website/src/constants/index.tsx
+++ website/src/constants/index.tsx
@@ -10,6 +10,6 @@ export const URLS: Urls = {
   github: 'https://github.com/ElioDiNino',
   linkedin: 'https://www.linkedin.com/in/eliodinino/',
   photography: 'https://photography.eliodinino.com',
-  instagram: 'https://www.instagram.com/elio.dinino/',
+  instagram: 'https://www.instagram.com/eliodinino/',
   resume: '/Resume.pdf',
 };
