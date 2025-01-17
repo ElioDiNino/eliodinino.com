176eab336e08e6bf586c8f6d6a7bc02c37e6f3d9
Elio Di Nino <52972357+ElioDiNino@users.noreply.github.com>
2025-01-16 22:46:58 -0800

    feat(Sentry): break out config and use custom router

--- website/src/index.tsx
+++ website/src/index.tsx
@@ -1,26 +1,9 @@
+import './instrument';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-
-import * as Sentry from '@sentry/react';
-
-Sentry.init({
-  dsn: 'https://0656a3ffc6ab7d8195bdf491eea907b3@o4508218594361344.ingest.us.sentry.io/4508495715303424',
-  environment: import.meta.env.MODE,
-  integrations: [
-    Sentry.browserTracingIntegration(),
-    Sentry.replayIntegration(),
-  ],
-  // Tracing
-  tracesSampleRate: 1.0,
-  // Which URLs distributed tracing should be enabled one
-  tracePropagationTargets: ['localhost', /^https:\/\/eliodinino\.com/, /^https:\/\/.*--eliodinino\.netlify\.app/],
-  // Session Replay
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
-});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
---
+++ website/src/instrument.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {
+  useLocation,
+  useNavigationType,
+  createRoutesFromChildren,
+  matchRoutes,
+} from 'react-router';
+
+import * as Sentry from '@sentry/react';
+
+Sentry.init({
+  dsn: 'https://0656a3ffc6ab7d8195bdf491eea907b3@o4508218594361344.ingest.us.sentry.io/4508495715303424',
+  environment: import.meta.env.MODE,
+  integrations: [
+    Sentry.replayIntegration(),
+    Sentry.reactRouterV7BrowserTracingIntegration({
+      useEffect: React.useEffect,
+      useLocation,
+      useNavigationType,
+      createRoutesFromChildren,
+      matchRoutes,
+    }),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0,
+  // Which URLs distributed tracing should be enabled one
+  tracePropagationTargets: [
+    'localhost',
+    /^https:\/\/eliodinino\.com/,
+    /^https:\/\/.*--eliodinino\.netlify\.app/,
+  ],
+  // Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
