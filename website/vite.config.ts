a3857e3f9af650653183f03565ed310f5ef3151c
Elio Di Nino <52972357+ElioDiNino@users.noreply.github.com>
2025-01-16 21:03:25 -0800

    feat(deps): Sentry Vite plugin

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,3 +1,4 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
@@ -5,9 +6,13 @@ import svgrPlugin from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+  plugins: [react(), viteTsconfigPaths(), svgrPlugin(), sentryVitePlugin({
+    org: "elio-org",
+    project: "eliodinino-com"
+  })],
   build: {
     outDir: 'build',
+    sourcemap: true
   },
   server: {
     open: true,
